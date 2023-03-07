179a1a6da7556f2611e792bf54caedda7e89de83
mikihogu <mk.i.ph93@gmail.com>
2023-03-08 03:37:40 +0900

    マイページ実装 FirstCommit

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,8 +1,22 @@
 class Public::MembersController < ApplicationController
-  def show
-  end
-  
+  before_action :authenticate_member!
 
+  def show
+    @member = Member.find(params[:id])
+  end
+
+  def edit
+    @member = Member.find(params[:id])
+  end
+
+  def update
+    @member = Member.find(params[:id])
+    if @member.update(member_params)
+      redirect_to member_path(current_member)
+    else
+      redirect_to request.referer
+    end
+  end
 
   private
   def member_params
--- app/models/member.rb
+++ app/models/member.rb
@@ -13,4 +13,13 @@ class Member < ApplicationRecord
 
   has_one_attached :image
   has_many_attached :dog_images
+
+  def get_image
+    (image.attached?) ? image: 'dog-no-image.jpeg'
+  end
+  
+  def get_dog_images
+    (dog_images.attached?) ? image: 'dog-no-image.jpeg'
+  end
+
 end
