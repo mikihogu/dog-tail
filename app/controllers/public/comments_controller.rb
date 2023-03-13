9860ef2fa915e6a77de240be7ae735ee51b5aee5
EC2 Default User <121223610+mikihogu@users.noreply.github.com>
2023-03-13 13:35:11 +0000

    コメント非同期通信化

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -5,12 +5,12 @@ class Public::CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     @comment.member_id = current_member.id
     @comment.post_id = @post.id
-    if @comment.save
-      @post.create_notification_comment!(current_member, @comment.id) #通知する
-      redirect_to request.referer
-    else
-      redirect_to request.referer
-    end
+    @comment.save
+    @post.create_notification_comment!(current_member, @comment.id) #通知する
+    #   redirect_to request.referer
+    # else
+    #   redirect_to request.referer
+    # end
   end
 
   def destroy
@@ -18,7 +18,7 @@ class Public::CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment.post_id = @post.id
     @comment.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   private
