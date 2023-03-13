1950f77bd7dbf4dc88ec803625cf237b3a5720ef
EC2 Default User <121223610+mikihogu@users.noreply.github.com>
2023-03-13 08:32:54 +0000

    interestとfavorite実装

--- app/controllers/public/interests_controller.rb
+++ app/controllers/public/interests_controller.rb
@@ -1,11 +1,21 @@
 class Public::InterestsController < ApplicationController
   def index
+    interests = Interest.where(member_id: current_member.id).pluck(:post_id)
+    @interest_posts = Post.find(interests)
   end
 
   def create
+    @post = Post.find(params[:post_id])
+    interest = current_member.interests.new(post_id:@post.id)
+    interest.save
+    redirect_to request.referer
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
+    interest = current_member.interests.find_by(post_id:@post.id)
+    interest.destroy
+    redirect_to request.referer
   end
 
 end
