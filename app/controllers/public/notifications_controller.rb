88bbd7326570a64f67a203b76085b1628ed66b4a
mikihogu <mk.i.ph93@gmail.com>
2023-03-14 04:05:58 +0900

    index未読/全部切替

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,26 +1,18 @@
 class Public::NotificationsController < ApplicationController
 
   def index
-    # @notifications = current_member.passive_notifications.page(params[:page]).per
-    @notifications = current_member.passive_notifications
-    # @notifications.where(checked: false).each do |notification|
-      # notification.update(checked: true)
-    # end
+    if params[:filter] == "unchecked"
+      @notifications = current_member.passive_notifications.where(checked: false)
+    else
+      @notifications = current_member.passive_notifications
+    end
   end
 
   def show
-    # visited_id == current_member.id
-    # @notification = Notification.find(params[:id])
-    # @comment = Comment.find(params[:comment_id])
-    #   comment.notification.update(checked: true)
-    # end
-    # @notification = Notification.find(params[:id])
-    # @post = @notification.comment.post
-    # @comment = @notification.comment
-
     @notification = Notification.find(params[:id])
     @comment = @notification.comment
     @post = @notification.post
     @notification.update(checked: true)
   end
+
 end
