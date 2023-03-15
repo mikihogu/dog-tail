8f088dfda506d63889493a09e5903570961c2a6a
EC2 Default User <121223610+mikihogu@users.noreply.github.com>
2023-03-15 16:28:01 +0000

    admin/会員詳細ページ

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -6,4 +6,15 @@ class Admin::MembersController < ApplicationController
   def show
     @member = Member.find(params[:id])
   end
+
+  def update
+    @member = Member.find(params[:id])
+    @member.update(member_params)
+    redirect_to request.referer
+  end
+
+  private
+  def member_params
+    params.require(:member).permit(:name, :nickname, :password, :email, :image, :dog, :introduction, dog_images: [])
+  end
 end
